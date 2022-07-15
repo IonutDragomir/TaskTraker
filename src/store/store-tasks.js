593508e10248f4437595b060df81ecbe76f4f383
Dragomir Ionut <ionutdragomir1410@yahoo.com>
2022-07-15 11:59:29 +0300

    update tasks

--- src/store/store-tasks.js
+++ src/store/store-tasks.js
@@ -4,23 +4,11 @@ import { uid } from 'quasar'
 const state = {
 	tasks: {
 		'ID1': {
-			name: 'Go to shop',
+			name: 'Create task',
 			completed: false,
 			dueDate: '2019/05/12',
 			dueTime: '18:30'
 		},
-		'ID2': {
-			name: 'Get bananas',
-			completed: false,
-			dueDate: '2019/05/13',
-			dueTime: '14:00'
-		},
-		'ID3': {
-			name: 'Get apples',
-			completed: false,
-			dueDate: '2019/05/14',
-			dueTime: '16:00'
-		}
 	},
 	search: '',
 	sort: 'name'
